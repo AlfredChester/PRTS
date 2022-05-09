de9edeb1c134bd1d65392bab480bffc369e1e932
FredB-mine <fredbao1126@gmail.com>
2022-05-09 09:44:17 +0800

    Inital Commit

--- complie.bat
+++ complie.bat
@@ -1,9 +1,4 @@
 copy X:\OS\PRTS_OS\PRTS.nas X:\OS\30天自制操作系统\tolset\RUNENVIRONMENT
 pushd X:\OS\30天自制操作系统\tolset\RUNENVIRONMENT
 pause
-asm
-pause
-.\run.bat
-pause
-copy X:\OS\30天自制操作系统\tolset\RUNENVIRONMENT\PRTS.img X:\OS\PRTS_OS
-pause
+asm | run
