ebc69053c932ebc2b75991161c7125859a919949
FredB-mine <fredbao1126@gmail.com>
2023-12-11 06:34:46 +0800

    main

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
-#![no_std] // 不链接 Rust 标准库
-#![no_main] // 禁用所有 Rust 层级的入口点
+#![no_std] // Not linking the Rust standard library
+#![no_main] // Ban all Rust level entries
 
-mod vga_buffer;
+mod vga_buffer; // VGA I/O support
 
 use core::panic::PanicInfo;
 
 #[no_mangle]
 // entry of the operating system
 pub extern "C" fn _start() -> ! {
-    for i in 0..=10000 {
-        println!("{}", i);
+    for i in 0..=100 {
+        println!("Loading: percentage={}%", i);
     }
+    println!("Permission: level 8.");
+    println!("Welcome back, doctor.");
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("Panicked at {}", _info);
     loop {}
 }
