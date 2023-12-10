3df83e258b8582ab4e9d3dbbb1597dfec7f158b4
FredB-mine <fredbao1126@gmail.com>
2023-12-10 19:04:26 +0800

    main

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,17 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[no_mangle]
+// entry of the operating system
 pub extern "C" fn _start() -> ! {
     use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", 114514 numbers: {}", 1919810).unwrap();
+    for i in 'a'..'z' {
+        write!(vga_buffer::WRITER.lock(), "{}", i).unwrap()
+    }
+    // write!(vga_buffer::WRITER.lock(), "{} Hello World", '\n').unwrap();
+    // write!(vga_buffer::WRITER.lock(), "114514 numbers: {}", 1919810).unwrap();
     loop {}
 }
 
-/// 这个函数将在 panic 时被调用
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
