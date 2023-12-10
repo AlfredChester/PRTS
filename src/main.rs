c26f34c973a7d60d41c3d11673367a873e067145
FredB-mine <fredbao1126@gmail.com>
2023-12-10 16:10:31 +0800

    Print 'Hello World' to vga cache

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,19 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // 不重整函数名
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // 因为链接器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
-    // 默认命名为 `_start`
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
