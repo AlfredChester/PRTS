a7d1abaf3efbb68f059582f287f59dffad08aad3
FredB-mine <fredbao1126@gmail.com>
2023-12-11 22:25:28 +0800

    main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 #![no_std] // Not linking the Rust standard library
 #![no_main] // Ban all Rust level entries
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
+mod serial;
 mod vga_buffer; // VGA I/O support
 
 use core::panic::PanicInfo;
 
+#[derive(Debug, Copy, Eq, PartialEq, Clone)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[no_mangle]
 // entry of the operating system
 pub extern "C" fn _start() -> ! {
-    for i in 0..=100 {
-        println!("Loading: percentage={}%", i);
-    }
-    println!("Permission: level 8.");
-    println!("Welcome back, doctor.");
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -20,4 +37,20 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("Panicked at {}", _info);
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
 }
